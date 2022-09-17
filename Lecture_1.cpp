d22a8f8a6158d1c1e915e546feec5b23de125df2
U1song <113780776+U1song@users.noreply.github.com>
2022-09-17 23:04:43 +0800

    Create Lecture_1.cpp

---
+++ Lecture_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int a = 5 + 10;
+	float b = 4.0 / 3;
+	char c = 99;
+	bool d = !false;
+	
+	cout<<a<<"\t"<<b<<"\n"<<c<<"\t"<<d<<"\n";
+	
+  return 0;
+}
